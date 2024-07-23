4c4a16f44c35283dc5bff10fe4a8394b0786be0c
OhHaneol <otyvs1109@hs.ac.kr>
2024-07-23 14:25:39 +0900

    build: 스프링 초기 환경 설정

---
+++ src/main/java/com/ormi/cookudasse/CookudasseApplication.java
@@ -0,0 +1,13 @@
+package com.ormi.cookudasse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CookudasseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CookudasseApplication.class, args);
+	}
+
+}
