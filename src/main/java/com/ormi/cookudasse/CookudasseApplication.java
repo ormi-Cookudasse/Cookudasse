e40bc1d279c414b0cbf299acc49279ce6c71f0a8
Haneol Oh <otyvs1109@hs.ac.kr>
2024-08-06 00:12:42 +0900

    fix: 타임존 서울로 변경 (#61)

--- src/main/java/com/ormi/cookudasse/CookudasseApplication.java
+++ src/main/java/com/ormi/cookudasse/CookudasseApplication.java
@@ -1,14 +1,49 @@
+//package com.ormi.cookudasse;
+//
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//
+//@SpringBootApplication
+//public class CookudasseApplication {
+//
+//	public static void main(String[] args) {
+//		SpringApplication.run(CookudasseApplication.class, args);
+//	}
+//
+//
+//}
+
+
 package com.ormi.cookudasse;
 
+import java.util.Arrays;
+import java.util.TimeZone;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.core.env.Environment;
 
+@Slf4j
 @SpringBootApplication
-public class CookudasseApplication {
+@RequiredArgsConstructor
+public class CookudasseApplication implements ApplicationListener<ApplicationReadyEvent> {
+	private final Environment environment;
 
 	public static void main(String[] args) {
+		init();
 		SpringApplication.run(CookudasseApplication.class, args);
 	}
 
+	@Override
+	public void onApplicationEvent(ApplicationReadyEvent event) {
+		log.info("applicationReady status" + Arrays.toString(environment.getActiveProfiles()));
+	}
 
+	private static void init() {
+		log.info("Spring Server TimeZone : Asia/Seoul");
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
 }
